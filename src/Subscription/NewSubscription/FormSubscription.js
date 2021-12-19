56306917fca6172c94bbffc6c9f275bb498b1ec5
kamlesh bannagare <kamleshbannagare10@gmail.com>
2021-12-19 20:30:15 +0530

    useReducer and useEffect used

--- src/Subscription/NewSubscription/FormSubscription.js
+++ src/Subscription/NewSubscription/FormSubscription.js
@@ -1,45 +1,72 @@
 import './FormSubscription.css';
-import { useState } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
+const formReducerFn = (latestState, action) => {
+    console.log("in form reducer fn:", latestState, action)
+    if (action.type === 'TITLE') {
+        return { ...latestState, userTitle: action.val }
+    }
+    else if (action.type === 'DATE') {
+        return { ...latestState, userDate: action.val }
+    }
+    else if (action.type === 'AMOUNT') {
+        return { ...latestState, userAmount: action.val }
+    }
+    return { userTitle: 'Enter Subscription Title', userDate: '', userAmount: 'Enter Amount' }
+}
 const FormSubscription = (props) => {
     // const [userTitle,setUserTitle]=useState("");
     // const [userDate,setUserDate]=useState("");
     // const [userAmount,setUserAmount]=useState("");
-    const [form, setForm] = useState({ userTitle: 'Enter Subscription Title', userDate: '', userAmount: 'Enter Amount' })
+    const [formReducer, setFormReducer] = useReducer(formReducerFn, { userTitle: 'Enter Subscription Title', userDate: '', userAmount: 'Enter Amount' })
+    // const [form, setForm] = useState({ userTitle: 'Enter Subscription Title', userDate: '', userAmount: 'Enter Amount' })
     const [isValid, setIsValid] = useState(true)
+    const { userTitle: titleReducer } = formReducer
+    useEffect(() => {
+        setTimeout(() => {
+            console.log("using Effect")
+            if (titleReducer.trim().length > 0) {
+                setIsValid(true)
+            }
+        }, 500)
+        return () => { console.log("cleanup function") }
+    }, [titleReducer])
     const titleChangeHandler = (events) => {
         //   setUserTitle(events.target.value)
         //   setForm({...form,userTitle:events.target.value})
-        if (events.target.value.trim().length > 0) {
-            setIsValid(true)
-        }
-        setForm((prevState) => {
-            return { ...prevState, userTitle: events.target.value }
-        })
-        console.log(form)
+        // if (events.target.value.trim().length > 0) {
+        //     setIsValid(true)
+        // }
+        setFormReducer({ type: 'TITLE', val: events.target.value })
+        // setForm((prevState) => {
+        //     return { ...prevState, userTitle: events.target.value }
+        // })
+
     }
     const dateChangeHandler = (events) => {
         // setForm({...form,userDate:events.target.value})
-        setForm((prevState) => {
-            return { ...prevState, userDate: events.target.value }
-        })
+        setFormReducer({ type: 'DATE', val: events.target.value })
+        // setForm((prevState) => {
+        //     return { ...prevState, userDate: events.target.value }
+        // })
         // setUserDate(events.target.value);
     }
     const amountChangeHandler = (events) => {
         // setForm({...form,userAmount:events.target.value}) 
-        setForm((prevState) => {
-            return { ...prevState, userAmount: events.target.value }
-        })
-        console.log(form)
+        setFormReducer({ type: 'AMOUNT', val: events.target.value })
+        // setForm((prevState) => {
+        //     return { ...prevState, userAmount: events.target.value }
+        // })
+
         // setUserAmount(events.target.value);
     }
     const submitHandler = (events) => {
         events.preventDefault();
-        if (form.userTitle.trim().length === 0) {
+        if (formReducer.userTitle.trim().length === 0) {
             setIsValid(false)
             return
         }
-        const Subscription = { title: form.userTitle, amount: form.userAmount, date: new Date(form.userDate) };
+        const Subscription = { title: formReducer.userTitle, amount: formReducer.userAmount, date: new Date(formReducer.userDate) };
         props.onSave(Subscription);
         console.log("on submit", Subscription)
     }
@@ -50,15 +77,15 @@ const FormSubscription = (props) => {
                     {/* {style={{ color: !isValid ? 'red' : 'black' }}} */}
                     {/* {style={{ borderColor: !isValid ? 'red' : 'black' }}} */}
                     <label>Title</label>
-                    <input type="text" value={form.userTitle} onChange={titleChangeHandler}></input>
+                    <input type="text" value={formReducer.userTitle} onChange={titleChangeHandler}></input>
                 </div>
                 <div className="new_subscription_control">
                     <label>Date</label>
-                    <input type="date" value={form.userDate} onChange={dateChangeHandler}></input>
+                    <input type="date" value={formReducer.userDate} onChange={dateChangeHandler}></input>
                 </div>
                 <div className="new_subscription_control">
                     <label>Amount</label>
-                    <input type="text" value={form.userAmount} onChange={amountChangeHandler}></input>
+                    <input type="text" value={formReducer.userAmount} onChange={amountChangeHandler}></input>
                 </div>
             </div>
             <div className="new_subscription_actions">
